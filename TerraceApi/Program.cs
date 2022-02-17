dd9b1dac453603c26d5845699159b92253a6da8e
Adam Chen <encodeous7@gmail.com>
2022-02-17 16:30:37 -0500

    Support reverse proxy

--- TerraceApi/Program.cs
+++ TerraceApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -23,6 +25,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
 app.UseCors("AnyOrigin");
 
