02f5d0ec12e40aa4264cd21a6cac0322dd972b69
Adam Chen <encodeous7@gmail.com>
2022-02-18 18:42:19 -0500

    Spinny :)

--- TerraceSite/src/routes/Home.svelte
+++ TerraceSite/src/routes/Home.svelte
@@ -1,47 +1,92 @@
 <script>
     import {API_URL} from "../js/api";
     import TypeOMatic from "../components/TypeOMatic.svelte";
+    import { onMount } from 'svelte';
 
-    const fetchImage = (async () => {
-        const response = await fetch(API_URL + "/WeatherForecast")
-        return await response.json()
-    })()
     const greetingName = "Hey There!\nI'm Adam.\nWelcome to my site!";
+
+    let degree = -20;
+    let newDegree = -20;
+    let velocity = 0;
+    let acceleration = 0;
+
+    $: blue_style = `
+        <style>
+        html, body{
+            background: linear-gradient(${degree + 90}deg, rgb(67,55,147) 12%, rgb(102,133,190) 45%, rgb(65, 83, 162) 100%);
+        }
+        </style>
+        `;
+
+    function handleMousemove(event) {
+        let x = window.innerWidth / 2;
+        let y = window.innerHeight / 2;
+        let nx = event.clientX;
+        let ny = event.clientY;
+        newDegree = Math.atan2(y - ny, x - nx)  * 180 / Math.PI;
+        newDegree %= 360;
+    }
+
+    onMount(async () => {
+        let rid = requestAnimationFrame(function update() {
+            degree = velocity + degree;
+            degree %= 360;
+            var delta = newDegree - degree;
+            if(Math.abs(delta) > 180){
+                if(delta < 0){
+                    delta = 360 + delta;
+                }else{
+                    delta = 360 - delta;
+                }
+            }
+            acceleration = delta / 10000;
+            velocity /= 1.14;
+            velocity += acceleration;
+            // console.log(acceleration + " " + velocity + " " + newDegree + " " + degree);
+            newDegree %= 360;
+            rid = requestAnimationFrame(update);
+        });
+        return () => cancelAnimationFrame(rid);
+    });
+
 </script>
 
-<div class="main-grid">
-    <div class="greeting">
-        <TypeOMatic text={greetingName}/>
-    </div>
-    <div class="extra-info">
-        <p>
-            This site is still under construction :)
-        </p>
+<svelte:window on:mousemove={handleMousemove}/>
+
+{@html blue_style}
+
+<div class="main-page">
+    <div class="main-grid">
+        <div class="greeting">
+            <TypeOMatic text={greetingName}/>
+        </div>
+        <div class="extra-info">
+            <p>
+                This site is still under construction :)
+            </p>
+        </div>
     </div>
 </div>
 
-
-
 <style lang="scss">
   @import "../styles/global";
-  :global(html, body){
-    @extend .terri-blue;
+  .main-page{
+    display: flex;
+    align-items: center;
+    height: 100%;
   }
   .main-grid{
     //@extend .tserra;
     @extend .fira;
     display: grid;
-    grid-template-columns: repeat(7, 1fr);
-    grid-template-rows: repeat(4, 1fr);
+    grid-template-rows: 1fr auto;
     grid-column-gap: 0;
     grid-row-gap: 0;
-    height: 100%;
+    padding: 80px;
   }
   .greeting {
-    grid-area: 2 / 2 / 4 / 4;
     display: inline-block;
   }
   .extra-info{
-    grid-area: 4 / 2 / 8 / 4;
   }
 </style>
